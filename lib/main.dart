b43bd47748c8c0959f890103c5911fa5eef5e7aa
Jonas Boone <boone.jonas@outlook.be>
2023-06-22 20:49:47 +0200

    refactor: move widget to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'widgets/word_card.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -58,35 +60,6 @@ class MyHomePage extends StatelessWidget {
               child: Text('Generate word'),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class WordCard extends StatelessWidget {
-  const WordCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displaySmall!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: '${pair.first}${pair.second}',
         ),
       ),
     );
